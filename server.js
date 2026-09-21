b3f782987a83b725f018be225861e97b959d8634
Pedro <joselop121794@gmail.com>
2026-09-21 01:47:26 -0600

    Actualizar server.js

--- server.js
+++ server.js
@@ -4,6 +4,33 @@ const app = express();
 // Middleware opcional
 app.use(express.json());
 
+// Listar usuarios
+app.get('/api/users', (req, res) => {
+  res.json([
+    { id: 1, name: 'Osvaldo' },
+    { id: 2, name: 'Valeria' }
+  ]);
+});
+
+// Crear usuario
+app.post('/api/users', (req, res) => {
+  const { name } = req.body;
+  res.json({ message: `Usuario ${name} creado` });
+});
+
+// Actualizar usuario
+app.put('/api/users/:id', (req, res) => {
+  const { id } = req.params;
+  const { name } = req.body;
+  res.json({ message: `Usuario ${id} actualizado a ${name}` });
+});
+
+// Eliminar usuario
+app.delete('/api/users/:id', (req, res) => {
+  const { id } = req.params;
+  res.json({ message: `Usuario ${id} eliminado` });
+});
+
 // Ruta de prueba
 app.get('/', (req, res) => {
   res.send('Servidor funcionando en Railway 🚀');
